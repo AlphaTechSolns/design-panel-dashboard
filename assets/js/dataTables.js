07b7174e97dfbbba288f787eb46cc01368867dfb
MathumithaArumugam <mathumithaarumugam@gmail.com>
2023-09-14 12:45:20 +0530

    refactoring

---
+++ assets/js/dataTables.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.dataTables .table').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ]
+    } );
+} );
